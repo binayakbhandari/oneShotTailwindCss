d7d90a253ffed7b57e4ffd7fccd5a744c8970573
binayakbhandari <binayakbhandari60@gmail.com>
2024-08-31 14:43:00 +0545

    push

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
       fontFamily : {
         domine : ['Domine','serif']
       },
+      animation : {
+        'rotating' : 'rotating 5s linear infinite'
+      },
+      keyframes : {
+        rotating : {
+          '0%' : {transform : 'rotate(0deg)'},
+          '25%' : {transform : 'rotate(90deg)'},
+          '50%' : {transform : 'rotate(180deg)'},
+          '75%' : {transform : 'rotate(270deg)'},
+          '100%' : {transform : 'rotate(360deg)'},
+        }
+      }
     },
   },
   plugins: [],
